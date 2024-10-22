b97452a50e28127e7b2786a48277ce1e6af691ba
Adrija De <133571784+Adri935@users.noreply.github.com>
2024-10-22 10:40:14 +0000

    Copilot second commit

---
+++ members.js
@@ -0,0 +1,7 @@
+function skillsMembers() {
+  return [
+    { name: 'John', skills: ['JS', 'CSS', 'HTML'] },
+    { name: 'Jane', skills: ['JS', 'Node', 'React'] },
+    { name: 'Doe', skills: ['JS', 'Node', 'React'] }
+  ];
+}
